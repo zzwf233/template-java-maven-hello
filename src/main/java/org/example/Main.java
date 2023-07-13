85f745c727cfba78b09a7c5d91067d868bda55ad
zzwf233 m <3343294579@qq.com>
2023-07-13 06:42:07 +0000

    ADD

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        DatabaseInitializer databaseInitializer = new DatabaseInitializer();
-        databaseInitializer.initializeDatabase();
         ShoppingManagementSystem system = new ShoppingManagementSystem();
         system.run();
     }
